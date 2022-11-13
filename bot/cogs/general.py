1c7fed96578d65b28406b588db149f1266e75fc9
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-11-13 16:47:25 +0530

    Create general.py

---
+++ bot/cogs/general.py
@@ -0,0 +1,11 @@
+import discord
+from bot.bot import Tournify
+from discord.ext import commands
+
+
+class General(commands.Cog, description="General Commands"):
+    def __init___(self, bot: Tournify)
+        self.bot = bot
+
+
+    
