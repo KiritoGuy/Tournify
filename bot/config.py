a4e2d2be75cf88deee5a0b9a8aba19a9b0d0dab7
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-11-13 16:50:44 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,3 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ.get("TOKEN")
+MAIN_COLOR = 0xFFD700
+RED_COLOR = 0xFF0000
+HEADER = "TOURNIFY | BRINGS UNDERDOGS ON TOP!"
+FOOTER = "TOURNIFY | POWERED BY TERKENAL ESPORTS"
