a9d8fb18c020e950384d9ca7e77d58622dde1456
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-11-13 17:26:43 +0530

    Update general.py

--- bot/cogs/general.py
+++ bot/cogs/general.py
@@ -4,8 +4,74 @@ from discord.ext import commands
 
 
 class General(commands.Cog, description="General Commands"):
-    def __init___(self, bot: Tournify)
+    def __init___(self, client: Tournify)
         self.bot = bot
 
 
-    
+    @commands.hybrid_command(name="serverinfo", help="information about server.", aliases=(['si']))
+    async def serverinfo(self, ctx: commands.Context):
+        guild: discord.Guild = ctx.guild
+        embed = discord.Embed(
+            title=f"Server Information",
+            description=f"Description: {guild.description}",
+            color=MAIN_COLOR
+        ).set_author(
+            name=guild.name,
+            icon_url=guild.me.display_avatar.url if guild.icon is None else guild.icon.url
+        ).set_footer(text=f"ID: {guild.id}")
+        if guild.icon is not None:
+            embed.set_thumbnail(url=guild.icon.url)
+        embed.add_field(
+            name="Basic Info:",
+            value=f"""
+**Owner:** <@{guild.owner_id}>
+**Created At:** <t:{round(time.time() - (datetime_to_seconds(guild.created_at) - time.time()))}:F>
+**Region:** {str(guild.region).title()}
+**System Channel:** {"None" if guild.system_channel is None else guild.system_channel.mention}
+**Verification Level:** {str(guild.verification_level).title()}
+            """,
+            inline=False
+        )
+        embed.add_field(
+            name="Members Info:",
+            value=f"""
+**Members:** `{len(guild.members)}`
+**Humans:** `{len(list(filter(lambda m: not m.bot, guild.members)))}`
+**Bots:** `{len(list(filter(lambda m: m.bot, guild.members)))}`
+            """,
+            inline=True
+        )
+        embed.add_field(
+            name="Channels Info:",
+            value=f"""
+**Categories:** `{len(guild.categories)}`
+**Text Channels:** `{len(guild.text_channels)}`
+**Voice Channels:** `{len(guild.voice_channels)}`
+**Threads:** `{len(guild.threads)}`
+            """,
+            inline=True
+        )
+        embed.add_field(
+            name="Other Info:",
+            value=f"""
+**Roles:** `{len(guild.roles)}`
+**Emojis:** `{len(guild.emojis)}`
+**Stickers:** `{len(guild.stickers)}`
+                """
+        )
+        if guild.features:
+            embed.add_field(
+                name="Features:",
+                value=', '.join([feature.replace('_', ' ').title() for feature in guild.features]),
+                inline=False
+            )
+        if guild.banner is not None:
+            embed.set_image(url=guild.banner.url)
+
+        return await ctx.reply(embed=embed)
+
+
+
+
+def setup(client):
+    client.add_cog(General(client))
